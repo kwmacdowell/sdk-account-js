9e311528c941587cbf9d3c469f79701e041e3663
Arie Trouw <arie.trouw@xyfindables.com>
2018-10-04 18:06:57 -0700

    added dist

---
+++ dist/index.js
@@ -0,0 +1,2 @@
+"use strict";var _account=_interopRequireDefault(require("./account"));require("core-js/modules/es6.object.define-property"),Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;function _interopRequireDefault(a){return a&&a.__esModule?a:{default:a}}var _default=_account.default;exports.default=_default;
+//# sourceMappingURL=index.js.map
